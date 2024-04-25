ac8efb9143a4dad333e06432079e6b3a49906d30
luzw3 <luzw3@lenovo.com>
2024-04-25 18:14:03 +0800

    modify model param for google ai

--- ncbot/plugins/google_ai/llms.py
+++ ncbot/plugins/google_ai/llms.py
@@ -10,14 +10,14 @@ import os
 plugin_name = 'google_ai'
 store = StorageStore.get_instance()
 api_key = os.getenv('GOOGLE_API_KEY')
-llm = ChatGoogleGenerativeAI(model_name='gemini-pro',api_key=api_key)
+llm = ChatGoogleGenerativeAI(model_name='gemini-pro',api_key=api_key, convert_system_message_to_human=True)
 
 @base.command(plname=plugin_name, funcname='chat',desc='Chat with model')
 def chat(userid, username, input):
     global llm
     model_id = store.get_key(get_user_model_id_key(userid))
     if model_id and model_id != llm.model:
-        llm = ChatGoogleGenerativeAI(model_id=model_id, api_key=api_key)
+        llm = ChatGoogleGenerativeAI(model_id=model_id, api_key=api_key, convert_system_message_to_human=True)
     
     return get_user_chat_response(llm, userid, input)
 
